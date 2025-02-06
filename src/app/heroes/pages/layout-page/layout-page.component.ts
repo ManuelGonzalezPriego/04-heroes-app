b5534b146084b73f00343b6a5642dac8417b1154
Manuel <manuelgonzalezpriego1@gmial.com>
2025-02-06 19:58:04 +0100

    9.1 Botones para el sidenav

--- src/app/heroes/pages/layout-page/layout-page.component.ts
+++ src/app/heroes/pages/layout-page/layout-page.component.ts
@@ -8,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class LayoutPageComponent {
 
+  //* Esta será nuestro diccionario con los labels, iconos y urls de nuestro sitio
+  public sidebarItems=[
+    {label:'Listado', icon: 'label', url: './list'},
+    {label:'Añadir', icon: 'add', url: './new-hero'},
+    {label:'Buscar', icon: 'search', url: './search'}
+  ]
 }
