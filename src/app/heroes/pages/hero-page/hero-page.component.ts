c70ac868b12cbe54ccdff4fe462956a398629925
Manuel <manuelgonzalezpriego1@gmail.com>
2025-02-18 02:12:54 +0100

    9.11 Página del Héroe

--- src/app/heroes/pages/hero-page/hero-page.component.ts
+++ src/app/heroes/pages/hero-page/hero-page.component.ts
@@ -1,2 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { delay, switchMap } from 'rxjs';
+import { HeroesService } from '../../services/heroes.service';
+import { Hero } from '../../interface/hero.interface';
 
 
+@Component({
+  selector: 'app-hero-page',
+  templateUrl: './hero-page.component.html',
+  styles: []
+})
+export class HeroPageComponent implements OnInit{
+public hero?:Hero
+
+  constructor(
+    private heroesService: HeroesService,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {
+  }
+
+  ngOnInit(): void {
+    console.log(this.activatedRoute.params);
+
+    this.activatedRoute.params.pipe(
+      delay(2000), //Vamos a introducir un delay dentro de la carga asíncrona para poder visualizar el spinner
+      switchMap(({ id }) => this.heroesService.getHeroById(id)) //Desestructuramos el el param y obtenemos el id para poder pasarlo al servicio
+    ).subscribe(hero => {
+      if(!hero) return this.router.navigate(['heroes/list']);
+
+      this.hero=hero;
+      console.log(hero);
+      return;
+      });
+
+  }
+
+}
