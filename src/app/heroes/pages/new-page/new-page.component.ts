5b1e39901d140073dabdef7b058d7f07b00fac1e
Manuel <manuelgonzalezpriego1@gmail.com>
2025-02-21 03:10:50 +0100

    10.2 Inicializar el formulario para editar

--- src/app/heroes/pages/new-page/new-page.component.ts
+++ src/app/heroes/pages/new-page/new-page.component.ts
@@ -2,6 +2,8 @@ import { Component } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { Hero, Publisher } from '../../interface/hero.interface';
 import { HeroesService } from '../../services/heroes.service';
+import { Router } from '@angular/router';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-new-page',
@@ -25,7 +27,9 @@ export class NewPageComponent {
     {"id": "Marvel Comics","desc": "Marvel - Comics"}
   ]
 
-  constructor(private heroesService:HeroesService){}
+  public hero:Hero=null!;
+
+  constructor(private heroesService:HeroesService, public router:Router){}
 
   get currentHero():Hero{
     return this.heroForm.value as Hero;
@@ -46,5 +50,31 @@ export class NewPageComponent {
       .subscribe(hero=>{
         // TODO: Mostrar snackbar y navegar a /hero/edit/hero.id
       });
+  }
+
+  ngOnInit():void{
+    if(!this.router.url.includes('edit')) return;
+
+    const urlSplit:string[]=this.router.url.split('/');
+    this.heroesService.getHeroById(urlSplit[urlSplit.length-1]).subscribe(hero=>{
+
+      // Angular mola mucho y como mola al poner ! delante del objeto heroe comprueva si es valido
+      if(!hero){
+        this.router.navigate(['/']);
+        return;
+      }
+
+      this.hero=hero;
+
+      this.heroForm.reset({
+        id: this.hero.id,
+        superhero: this.hero.superhero,
+        publisher: this.hero.publisher,
+        alter_ego: this.hero.alter_ego,
+        first_appearance: this.hero.first_appearance,
+        characters: this.hero.characters,
+        alt_img: this.hero.alt_img
+      });
+    });
   }
 }
